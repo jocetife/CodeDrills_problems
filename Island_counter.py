9b0a7428e21ec2e222bd0ad184f9e2885c2f37d3
Jocelyn <jocelyntiradofelix@gmail.com>
2025-04-12 12:33:20 -0600

    structure

---
+++ Island_counter.py
@@ -0,0 +1,6 @@
+class IslandCount:
+	# DO NOT read from stdin or write to stdout.
+	# Input is given as the function arguments.
+	# Output is taken as the function return value.
+	def getCount(self, grid: list[str]) -> int:
+		pass # Code here
